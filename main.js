7562c0ff2caca2872f33d00610a78e6bb4d91ed0
DotoryHamsta <167158735+DotoryHamsta@users.noreply.github.com>
2026-06-05 20:20:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ document.body.style.margin = '0';
 
 const mount = document.getElementById('mapMount');
 
-document.body.insertAdjacentHTML(
-  'afterbegin',
-  '<div style="position:fixed;top:0;left:0;z-index:99999;background:yellow;color:black;padding:10px;font-size:24px;">MAIN.JS TOP LEVEL OK</div>'
-);
-
 if (!mount) {
   document.body.innerHTML = '<div style="padding:24px;color:red;font-size:32px;">NO MOUNT</div>';
   throw new Error('No mapMount element found.');
