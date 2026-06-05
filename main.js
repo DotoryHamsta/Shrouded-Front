3ff3000787132899b23b25b12114f39eac84e0c7
DotoryHamsta <167158735+DotoryHamsta@users.noreply.github.com>
2026-06-05 19:37:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,4 @@ document.body.insertAdjacentHTML(
 
 if (mount) {
   mount.innerHTML = '<div style="padding:24px;font-size:32px;color:white;">MAIN.JS WORKS</div>';
-} else {
-  document.body.innerHTML = '<div style="padding:24px;font-size:32px;color:red;">NO MOUNT</div>';
 }
