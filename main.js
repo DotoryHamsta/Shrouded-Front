d7963bcb4f4212c50400e2a10378a2be9df006ac
DotoryHamsta <167158735+DotoryHamsta@users.noreply.github.com>
2026-06-05 19:27:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-document.getElementById('mapMount').innerHTML = `
-  <div style="padding:24px; font-size:32px; color:white;">
-    MAIN.JS WORKS
-  </div>
-`;
+const mount = document.getElementById('mapMount');
+
+if (mount) {
+  mount.innerHTML = '<div style="padding:24px;font-size:32px;color:white;">MAIN.JS WORKS</div>';
+} else {
+  document.body.innerHTML = '<div style="padding:24px;font-size:32px;color:red;">NO MOUNT</div>';
+}
